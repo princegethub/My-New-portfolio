222eb6ab07e6c11f2d9a08345dc32ceeae9409e6
prince mishra <pm618209@gmail.com>
2024-08-15 13:14:51 +0530

    problem solving

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let darkMode = document.querySelector("#dark_mode");
-<<<<<<< HEAD
 let data = 0; // 0 for light mode, 1 for dark mode
 
 let tikTak = document.querySelector("#tiktak_menu");
@@ -9,18 +8,11 @@ let tikTak_data = 0; // Initialize the variable
 // Dark Mode Toggle
 darkMode.addEventListener("click", () => {
   console.log("Dark mode toggled");
-=======
-let data = 0;
-
-darkMode.addEventListener("click", () => {
-  console.log("you clicked me");
->>>>>>> origin/main
 
   if (data === 0) {
     darkMode.classList.remove("fa-moon");
     darkMode.classList.add("fa-sun");
     data = 1;
-<<<<<<< HEAD
     document.body.classList.add('dark');
   } else {
     darkMode.classList.remove("fa-sun");
@@ -44,17 +36,5 @@ tikTak.addEventListener("click", () => {
     tikTak.classList.add("fa-bars");
     tikTak_data = 0;
     linkContainer.style.right = '-50px';
-=======
-    document.body.classList.add('dark')
-
-
-  } else {
-
-    darkMode.classList.remove("fa-sun");
-    darkMode.classList.add("fa-moon");
-    data = 0;
-    document.body.classList.remove('dark')
-
->>>>>>> origin/main
   }
 });
